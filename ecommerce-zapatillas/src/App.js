b013ca87ff30f0e02d59b4014b7cd6dfef5a5a7f
Santi Centurion <santiagocenturion77@gmail.com>
2022-11-03 00:22:42 -0300

    subida del proyecto

---
+++ ecommerce-zapatillas/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { MyRouts } from './routes/routes'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Inicio </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ ecommerce-zapatillas/src/routes/routes.js
@@ -0,0 +1,10 @@
+import { Routes, Route, Link } from 'react-router-dom';
+
+
+import function MyRouts(){
+    return (
+        <Routes>
+            <Route path='/' />
+        </Routes>
+    )
+}
